72e2bd323d972c08a1e0caf34d78d3306c198522
Keijo Korhonen <kkorhonenk.k@gmail.com>
2019-06-11 18:34:04 +0300

    osa4a loppuun

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = blogs => {
     return 1
 }
@@ -14,7 +16,11 @@ const totalLikes = blogs => {
 
 const favoriteBlog = blogs => {
     if (blogs.length === 0) {
-        return {}
+        return {
+            title: undefined,
+            author: undefined,
+            likes: undefined
+        }
     }
 
     var mostLikes = blogs[0].likes
@@ -27,11 +33,62 @@ const favoriteBlog = blogs => {
         }
     }
 
-    return blogs[indexOfFavorite]
+    return {
+        title: blogs[indexOfFavorite].title,
+        author: blogs[indexOfFavorite].author,
+        likes: blogs[indexOfFavorite].likes
+    }
+}
+
+const mostBlogs = blogs => {
+    if (blogs.length === 0) {
+        return {
+            author: undefined,
+            blogs: undefined
+        }
+    }
+
+    const authorsSortedByCount = _.chain(blogs.map(blog => blog.author)).countBy().value()
+
+    return {
+        author: _.findLastKey(authorsSortedByCount),
+        blogs: _.findLast(authorsSortedByCount)
+    }
+}
+
+const mostLikes = blogs => {
+    if (blogs.length === 0) {
+        return {
+            author: undefined,
+            likes: undefined
+        }
+    }
+
+    var authorsAndLikes = []
+
+    _.chain(blogs)
+        .groupBy('author')
+        .forEach(author => {
+            const likes = _.sumBy(author, 'likes')
+            authorsAndLikes.push({
+                author: author[0].author,
+                likes: likes
+            })
+        })
+        .value()
+
+    const mostLikedAuthor = _.maxBy(authorsAndLikes, 'likes')
+
+    return {
+        author: mostLikedAuthor.author,
+        likes: mostLikedAuthor.likes
+    }
 }
 
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
