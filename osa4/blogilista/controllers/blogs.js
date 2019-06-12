e694502375d2e51ddc50e1f982ec5127efc0a5e5
Keijo Korhonen <kkorhonenk.k@gmail.com>
2019-06-13 00:46:02 +0300

    osa4.13-14

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,25 +1,47 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response, next) => {
     const blog = new Blog(request.body)
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-        .catch(error => {
-            next(error)
-        })
+    try {
+        const savedBlog = await blog.save()
+        response.status(201).json(savedBlog)
+    } catch (exception) {
+        next(exception)
+    }
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+    try {
+        await Blog.findByIdAndRemove(request.params.id)
+        response.status(204).end()
+    } catch (exception) {
+        next(exception)
+    }
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+    const body = request.body
+
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    }
+
+    try {
+        const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+        response.json(updatedBlog.toJSON())
+    } catch (exception) {
+        next(exception)
+    }
 })
 
 module.exports = blogsRouter
--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -6,88 +6,125 @@ const helper = require('./test_helper')
 
 const Blog = require('../models/blog')
 
-beforeEach(async () => {
-    await Blog.remove({})
+describe('when there is initially some notes saved', () => {
 
-    const blogObjects = helper.initialBlogs
-        .map(blog => new Blog(blog))
-    const promiseArray = blogObjects.map(blog => blog.save())
-    await Promise.all(promiseArray)
-})
 
-test('blogs are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-})
+    beforeEach(async () => {
+        await Blog.remove({})
 
-test('blogs contain field called id', async () => {
-    const res = await api.get('/api/blogs')
-    expect(res.body[0].id).toBeDefined()
-})
+        const blogObjects = helper.initialBlogs
+            .map(blog => new Blog(blog))
+        const promiseArray = blogObjects.map(blog => blog.save())
+        await Promise.all(promiseArray)
+    })
 
-test('a valid blog can be added', async () => {
-    const newBlog = {
-        title: 'Top JavaScript VSCode Extensions for Faster Development',
-        author: 'Arfat Salman',
-        url: 'https://codeburst.io/top-javascript-vscode-extensions-for-faster-development-c687c39596f5',
-        likes: 6
-    }
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+    test('blogs contain field called id', async () => {
+        const res = await api.get('/api/blogs')
+        expect(res.body[0].id).toBeDefined()
+    })
 
-    const blogsAtEnd = await api.get('/api/blogs')
-    expect(blogsAtEnd.body.length).toBe(helper.initialBlogs.length + 1)
+    test('a valid blog can be added', async () => {
+        const newBlog = {
+            title: 'Top JavaScript VSCode Extensions for Faster Development',
+            author: 'Arfat Salman',
+            url: 'https://codeburst.io/top-javascript-vscode-extensions-for-faster-development-c687c39596f5',
+            likes: 6
+        }
 
-    const titles = blogsAtEnd.body.map(blog => blog.title)
-    expect(titles).toContain(
-        'Top JavaScript VSCode Extensions for Faster Development'
-    )
-})
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
 
-test('undefined likes are set to zero', async () => {
-    const newBlog = {
-        title: 'Top JavaScript VSCode Extensions for Faster Development',
-        author: 'Arfat Salman',
-        url: 'https://codeburst.io/top-javascript-vscode-extensions-for-faster-development-c687c39596f5'
-    }
+        const blogsAtEnd = await api.get('/api/blogs')
+        expect(blogsAtEnd.body.length).toBe(helper.initialBlogs.length + 1)
 
-    const response = await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+        const titles = blogsAtEnd.body.map(blog => blog.title)
+        expect(titles).toContain(
+            'Top JavaScript VSCode Extensions for Faster Development'
+        )
+    })
 
-    expect(response.body.likes).toBe(0)
-})
+    test('undefined likes are set to zero', async () => {
+        const newBlog = {
+            title: 'Top JavaScript VSCode Extensions for Faster Development',
+            author: 'Arfat Salman',
+            url: 'https://codeburst.io/top-javascript-vscode-extensions-for-faster-development-c687c39596f5'
+        }
 
-test('invalid blog requests are responded to with bad request', async () => {
-    const newBlogWithoutTitle = {
-        author: 'Arfat Salman',
-        url: 'https://codeburst.io/top-javascript-vscode-extensions-for-faster-development-c687c39596f5',
-        likes: 6
-    }
+        const response = await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
 
-    const newBlogWithoutURL = {
-        title: 'Top JavaScript VSCode Extensions for Faster Development',
-        author: 'Arfat Salman',
-        likes: 6
-    }
+        expect(response.body.likes).toBe(0)
+    })
 
-    await api
-        .post('/api/blogs')
-        .send(newBlogWithoutTitle)
-        .expect(400)
+    test('invalid blog requests are responded to with bad request', async () => {
+        const newBlogWithoutTitle = {
+            author: 'Arfat Salman',
+            url: 'https://codeburst.io/top-javascript-vscode-extensions-for-faster-development-c687c39596f5',
+            likes: 6
+        }
 
-    await api
-        .post('/api/blogs')
-        .send(newBlogWithoutURL)
-        .expect(400)
+        const newBlogWithoutURL = {
+            title: 'Top JavaScript VSCode Extensions for Faster Development',
+            author: 'Arfat Salman',
+            likes: 6
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(newBlogWithoutTitle)
+            .expect(400)
+
+        await api
+            .post('/api/blogs')
+            .send(newBlogWithoutURL)
+            .expect(400)
+    })
+
+    test('specified blogs can be removed', async () => {
+        const blogsAtStart = await api.get('/api/blogs')
+        const blogToDelete = blogsAtStart.body[0]
+
+        await api
+            .delete(`/api/blogs/${blogToDelete.id}`)
+            .expect(204)
+
+        const blogsAtEnd = await api.get('/api/blogs')
+
+        expect(blogsAtEnd.body.length).toBe(helper.initialBlogs.length - 1)
+
+        const titles = blogsAtEnd.body.map(blog => blog.title)
+        expect(titles).not.toContain(blogToDelete.title)
+    })
+
+    test('blog can be updated', async () => {
+        const blogsAtStart = await api.get('/api/blogs')
+        const blogToUpdate = { ...blogsAtStart.body[0], likes: blogsAtStart.body[0].likes + 1 }
+
+        await api
+            .put(`/api/blogs/${blogToUpdate.id}`)
+            .send(blogToUpdate)
+            .expect(200)
+
+        const blogsAtEnd = await api.get('/api/blogs')
+
+        expect(blogsAtEnd.body.length).toBe(helper.initialBlogs.length)
+
+        const updatedBlog = blogsAtEnd.body.find(blog => blog.id === blogToUpdate.id)
+        expect(updatedBlog.likes).toBe(blogToUpdate.likes)
+    })
 })
 
 afterAll(() => {
