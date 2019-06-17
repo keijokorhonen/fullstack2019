d7f1b546f15eb6a5de869443d24d6bb86533fb54
Keijo Korhonen <kkorhonenk.k@gmail.com>
2019-06-17 16:59:48 +0300

    try catch for backend login controller

--- osa4/blogilista/controllers/login.js
+++ osa4/blogilista/controllers/login.js
@@ -4,29 +4,34 @@ const loginRouter = require('express').Router()
 const User = require('../models/user')
 
 loginRouter.post('/', async (request, response) => {
-    const body = request.body
+    try {
+        const body = request.body
 
-    const user = await User.findOne({ username: body.username })
-    const passwordCorrect = user === null
-        ? false
-        : await bcrypt.compare(body.password, user.passwordHash)
+        const user = await User.findOne({ username: body.username })
+        const passwordCorrect = user === null
+            ? false
+            : await bcrypt.compare(body.password, user.passwordHash)
 
-    if (!(user && passwordCorrect)) {
-        return response.status(401).json({
-            error: 'invalid username or password'
-        })
+        if (!(user && passwordCorrect)) {
+            return response.status(401).json({
+                error: 'invalid username or password'
+            })
+        }
+
+        const userForToken = {
+            username: user.username,
+            id: user._id,
+        }
+
+        const token = jwt.sign(userForToken, process.env.SECRET)
+
+        response
+            .status(200)
+            .send({ token, username: user.username, name: user.name })
+    } catch (exception) {
+        console.log(exception.message)
     }
 
-    const userForToken = {
-        username: user.username,
-        id: user._id,
-    }
-
-    const token = jwt.sign(userForToken, process.env.SECRET)
-
-    response
-        .status(200)
-        .send({ token, username: user.username, name: user.name })
 })
 
 module.exports = loginRouter
