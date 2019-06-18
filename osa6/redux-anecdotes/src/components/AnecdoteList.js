4114ee73094ef838abbf57112eb83642c9ccaee7
Keijo Korhonen <kkorhonenk.k@gmail.com>
2019-06-18 15:09:14 +0300

    osa6.7-8

---
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { voteAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = (props) => {
+    const anecdotes = props.store.getState()
+    const store = props.store
+    
+    const vote = (id) => {
+      console.log('vote', id)
+      store.dispatch(voteAnecdote(id))
+    }
+
+    return (
+        <div>
+            {anecdotes.sort((a, b) => b.votes - a.votes)
+                .map(anecdote =>
+                    <div key={anecdote.id}>
+                        <div>
+                            {anecdote.content}
+                        </div>
+                        <div>
+                            has {anecdote.votes}
+                            <button onClick={() => vote(anecdote.id)}>vote</button>
+                        </div>
+                    </div>
+                )}
+        </div>
+    )
+}
+
+export default AnecdoteList
