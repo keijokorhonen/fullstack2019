5eed89f9cd13bce2561f0af7bd61926befbe7411
Keijo Korhonen <kkorhonenk.k@gmail.com>
2019-06-19 15:37:16 +0300

    osa6.9

---
+++ osa6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,10 @@
+const initialState = 'initial notification'
+
+const noteReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+export default noteReducer
